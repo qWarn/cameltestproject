99d61927111c2814f642f6eca328dbeea318eb37
qWarn <kirilov-nikitka@list.ru>
2024-02-28 19:33:13 +0300

    Updated ActivemqRoute

--- src/main/java/com/example/camelproject/routes/ActivemqRoute.java
+++ src/main/java/com/example/camelproject/routes/ActivemqRoute.java
@@ -1,36 +1,47 @@
 package com.example.camelproject.routes;
 
 import com.example.camelproject.configs.ActivemqProperties;
+import com.example.camelproject.models.Person;
+import com.example.camelproject.processors.HttpOperationFailedProcessor;
 import com.example.camelproject.sevices.PersonService;
 import lombok.RequiredArgsConstructor;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
-import org.springframework.stereotype.Component;
-@Component
+import org.apache.camel.component.jackson.JacksonDataFormat;
+import org.apache.camel.http.base.HttpOperationFailedException;
+
 @RequiredArgsConstructor
 public class ActivemqRoute extends RouteBuilder {
 
     private final PersonService personService;
-
     private final ActivemqProperties activemqProperties;
+    private final JacksonDataFormat format = new JacksonDataFormat(Person.class);
+    private final HttpOperationFailedProcessor httpOperationFailedProcessor;
+
 
     /**
      * Route, that is listening to Activemq queue.
-     * When new message is added it saves it into database and then sends to http://localhost:8080/savedUser in header "Person".
+     * When new message is added it sends it to http://localhost:8080/save/user.
      *
      * @throws Exception
      */
     @Override
     public void configure() throws Exception {
         from("activemq:" + activemqProperties.getCamelQueueName())
-                .onException(RuntimeException.class)
-                    .setBody(simple("${exception.message}"))
-                    .log(LoggingLevel.WARN, "${body}")
-                    .handled(true)
+                .onException(HttpOperationFailedException.class)
+                .process(httpOperationFailedProcessor)
+                .log(LoggingLevel.ERROR, "Caught HttpOperationFailedException with body: ${body}")
+                .handled(true)
+                .end()
+                .onException(Exception.class)
+                .setBody(simple("${exception.message}"))
+                .log(LoggingLevel.ERROR, "Exception due proceeding Activemq route: ${body}")
+                .handled(true)
                 .end()
                 .routeId("Activemq route")
                 .log(LoggingLevel.INFO, "Got json from Activemq: ${body}")
-                .process(personService::savePersonAndSetItToExchange)
-                .to("http://localhost:8080/savedUser?httpMethod=GET");
+                .process(personService::setPersonToExchange)
+                .marshal(format)
+                .to("http://localhost:8080/save/user?httpMethod=POST");
     }
 }
