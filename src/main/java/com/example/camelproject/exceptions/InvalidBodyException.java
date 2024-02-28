ae2d81d62f3d1d1b42322e21ad073c3ba1819828
qWarn <kirilov-nikitka@list.ru>
2024-02-28 19:34:12 +0300

    Removed InvalidBodyException.java

--- src/main/java/com/example/camelproject/exceptions/InvalidBodyException.java
+++
@@ -1,9 +0,0 @@
-package com.example.camelproject.exceptions;
-
-public class InvalidBodyException extends RuntimeException{
-
-    public InvalidBodyException(String message) {
-        super(message);
-    }
-
-}
