ff3f746b7d7ace18e1dd4a2b54a224bc64c40bed
qWarn <kirilov-nikitka@list.ru>
2024-02-28 19:34:43 +0300

    Updated Person

--- src/main/java/com/example/camelproject/models/Person.java
+++ src/main/java/com/example/camelproject/models/Person.java
@@ -6,12 +6,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.io.Serializable;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Person {
+public class Person implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
