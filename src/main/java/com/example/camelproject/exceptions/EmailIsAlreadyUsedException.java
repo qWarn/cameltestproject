58199a869ef23d123770f548821cb406e5fecd90
qWarn <kirilov-nikitka@list.ru>
2024-02-28 19:33:55 +0300

    Updated EmailIsAlreadyUsedException

--- src/main/java/com/example/camelproject/exceptions/EmailIsAlreadyUsedException.java
+++ src/main/java/com/example/camelproject/exceptions/EmailIsAlreadyUsedException.java
@@ -1,5 +1,9 @@
 package com.example.camelproject.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class EmailIsAlreadyUsedException extends RuntimeException{
 
     public EmailIsAlreadyUsedException(String message) {
