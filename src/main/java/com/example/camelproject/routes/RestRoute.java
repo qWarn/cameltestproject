a0ae4765f8e3fae92b26d9cfe09e7ac8cb8aa68f
qWarn <kirilov-nikitka@list.ru>
2024-02-28 19:35:43 +0300

    Updated RestRoute

--- src/main/java/com/example/camelproject/routes/RestRoute.java
+++ src/main/java/com/example/camelproject/routes/RestRoute.java
@@ -1,11 +1,13 @@
 package com.example.camelproject.routes;
 
+import com.example.camelproject.models.Person;
 import com.example.camelproject.sevices.PersonService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jackson.JacksonDataFormat;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,10 +15,11 @@ import org.springframework.stereotype.Component;
 public class RestRoute extends RouteBuilder {
 
     private final PersonService personService;
+    private final JacksonDataFormat format = new JacksonDataFormat(Person.class);
 
     /**
-     * Route, which handles requests on http://localhost:8080/service/restRoute consumes application/json.
-     * Then sends body to http://localhost:8080/user?userId=id.
+     * Route, which handles requests on http://localhost:8080/service/route consumes application/json.
+     * Then sends body to http://localhost:8080/user.
      *
      * @throws Exception
      * @Example:{ "id":1,
@@ -27,20 +30,21 @@ public class RestRoute extends RouteBuilder {
      */
     @Override
     public void configure() throws Exception {
-        from("rest:get:restRoute?host=localhost:8080&consumes=application/json")
+        from("rest:get:route?host=localhost:8080&consumes=application/json")
                 .routeId("Rest route")
-                .log(LoggingLevel.INFO, "Got body: ${body}")
                 .doTry()
-                    .process(personService::setPersonAndIdToExchange)
-                    .toD("http://localhost:8080/user?userId=${header.userId}&bridgeEndpoint=true&httpMethod=GET")
-                    .setBody(constant("Success"))
+                .unmarshal(format)
+                .process(personService::setPersonToExchange)
+                .marshal(format)
+                .setHeader(Exchange.HTTP_METHOD, simple("PATCH"))
+                .to("http://localhost:8080/user?bridgeEndpoint=true")
                 .doCatch(JsonProcessingException.class)
-                    .log(LoggingLevel.ERROR, "Invalid body ${body}")
-                    .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(400))
-                    .setBody(simple("Invalid body ${body}"))
+                .log(LoggingLevel.ERROR, "Invalid body ${body}")
+                .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(400))
+                .setBody(simple("Invalid body ${body}"))
                 .doCatch(Exception.class)
-                    .setBody(simple("${exception.message}"))
-                    .log(LoggingLevel.ERROR, "${exception.message}")
+                .setBody(simple("${exception.message}"))
+                .log(LoggingLevel.ERROR, "${exception.message}")
                 .end();
     }
 }
